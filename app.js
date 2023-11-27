9f537cc0d2d2f55852964da159becc8b1d3d6180
obaloluwa28 <oduyemi.obaloluwa@gmail.com>
2023-11-27 12:40:14 +0100

    test commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const ErrorHandler = require("./middleware/error");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:3001'],
+
+  methods: [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE'
+  ],
+
+  credentials: true,
+
+  allowedHeaders: [
+    'Content-Type',
+  ]
+};
+
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+app.use("/", express.static("uploads"));
+
+app.use(express.json({limit: '500mb'}));
+app.use(express.urlencoded({
+  limit: '500mb', 
+  parameterLimit: 100000000, 
+  extended: true
+}));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+// import routes
+const user = require("./controller/user");
+const shop = require("./controller/shop");
+const product = require("./controller/product");
+const event = require("./controller/event");
+const coupon = require("./controller/coupounCode");
+const payment = require("./controller/payment");
+const order = require("./controller/order");
+const conversation = require("./controller/conversation");
+const message = require("./controller/message");
+const withdraw = require("./controller/withdraw");
+
+app.use("/api/v2/user", user);
+app.use("/api/v2/conversation", conversation);
+app.use("/api/v2/message", message);
+app.use("/api/v2/order", order);
+app.use("/api/v2/shop", shop);
+app.use("/api/v2/product", product);
+app.use("/api/v2/event", event);
+app.use("/api/v2/coupon", coupon);
+app.use("/api/v2/payment", payment);
+app.use("/api/v2/withdraw", withdraw);
+
+// it's for ErrorHandling
+app.use(ErrorHandler);
+
+module.exports = app;
