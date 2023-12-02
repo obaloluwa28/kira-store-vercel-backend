555590b734613cf938f1f557cd4a19ce5f005ee3
obaloluwa28 <oduyemi.obaloluwa@gmail.com>
2023-12-02 10:56:38 +0100

    changed email message

--- controller/shop.js
+++ controller/shop.js
@@ -44,7 +44,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
     try {
       await sendMail({
         email: seller.email,
-        subject: "Activate your Shop",
+        subject: "Account Activation",
         message: `Dear ${seller.name}, \n\nKindly click on the link to activate your shop's account: ${activationUrl}. This link expires in 5 minutes`,
       });
       res.status(201).json({
