f85cbb9531ad16c5d5677fcbbac36b31ac496250
obaloluwa28 <oduyemi.obaloluwa@gmail.com>
2024-10-24 03:11:26 +0100

    reverted back to the od site cookies

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,23 +2,23 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
-  // // Options for cookies
-  // const options = {
-  //   expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-  //   httpOnly: true,
-  //   sameSite: "none",
-  //   secure: true,
-  // };
-
   // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-    domain: ".kirasurf.com", // This allows subdomains to share the cookie
-    path: "/",
-    httpOnly: true, // Optional: Set this if you don't need to access the cookie via JavaScript
-    secure: true, // Make sure this is true in production (requires HTTPS)
-    sameSite: "none", // Adjust based on whether you're doing cross-site requests
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
   };
+
+  // Options for cookies
+  // const options = {
+  //   expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+  //   domain: ".kirasurf.com", // This allows subdomains to share the cookie
+  //   path: "/",
+  //   httpOnly: true, // Optional: Set this if you don't need to access the cookie via JavaScript
+  //   secure: true, // Make sure this is true in production (requires HTTPS)
+  //   sameSite: "none", // Adjust based on whether you're doing cross-site requests
+  // };
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
